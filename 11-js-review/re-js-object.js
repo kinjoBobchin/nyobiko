71f3ba1aa43b06b61f525738e86c4442d5446600
金城 裕也 <kinjou_yuya@ca-adv.co.jp>
2018-01-22 11:04:29 +0900

    ボタンを特定

--- 11-js-review/re-js-object.js
+++ 11-js-review/re-js-object.js
@@ -5,8 +5,9 @@
     //表示する画面の選択
     var display = document.getElementById("display-area");
     //startボタンを実装
-    var stop = document.getElementById("display-area");
+    var start = document.getElementById("start");
     //stopボタンを実装
+    var stop = document.getElementById("stop");
     //機能の定義：
     //①ボタンを押すと、時間が開始
     //②画面に、現在の時間が表示され、ダイアログで5秒で止めようと表示する
