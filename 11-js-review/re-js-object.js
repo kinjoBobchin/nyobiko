b8fa3465c680ea9dbfec7a3c94db537a336f2580
金城 裕也 <kinjou_yuya@ca-adv.co.jp>
2018-01-22 11:50:36 +0900

    あれ、スタートボタンが反応していない

--- 11-js-review/re-js-object.js
+++ 11-js-review/re-js-object.js
@@ -2,16 +2,28 @@
 
 (function(){
     'use strict';
-    //表示する画面の選択
+
+    //時間を表示する場所
     var display = document.getElementById("display-area");
-    //startボタンを実装
-    var start = document.getElementById("start");
-    //stopボタンを実装
-    var stop = document.getElementById("stop");
+    //startボタン
+    var startButton = document.getElementById("start-button");
+    //stopボタン
+    var stopButton = document.getElementById("stop-button");
+
     //機能の定義：
     //①ボタンを押すと、時間が開始
     //②画面に、現在の時間が表示され、ダイアログで5秒で止めようと表示する
     //③ストップボタンを押す
     //④誤差-0.5秒以内→「すごい」、ちょうど「凄すぎワロタ」、それ以外→「おしい」
+
+    //答えとのギャップ、答え→start,stopのファンクションを作っている、自分→いきなりfunctionの中にまとめようとしている
+    //いろいろ、機能を作る→あとでオブジェクトとしてまとめたほうが良さそう
+    function start() {
+        // var currentTime = new Date().getTime;
+        // display.innerText = currentTime;
+        console.log("ボタン反応してるよ");
+    }
+    startButton.click(start);
+
 
 })();
