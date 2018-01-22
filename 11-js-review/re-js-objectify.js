a01ac01850982d506385aee2c38b2f7fb489a351
bobchin <kinjo.bobchin@gmail.com>
2018-01-23 00:54:56 +0900

    showDisplayをgameオブジェクトのプロパティに登録完了。game.〜ではじまるやつを消したらエラーになたので、消せないようだ

--- 11-js-review/re-js-objectify.js
+++ 11-js-review/re-js-objectify.js
@@ -11,30 +11,41 @@
         initTime: null, //初期の時間
         countUpTime: null, //カウントアップする時間
         countUpId: null, //countUpIdを定義して、カウントしている時間を止める
-        timeToAdd: null //スタートの時間 - ストップの時間を足し上げて、ストップ機能を実装→https://dotinstall.com/lessons/stop_watch_js_v4/41207
+        timeToAdd: null, //スタートの時間 - ストップの時間を足し上げて、ストップ機能を実装→https://dotinstall.com/lessons/stop_watch_js_v4/41207
+
+        //表示する時間
+        showDisplay: function(time) {
+            if (0 >= time) {
+                game.display.innerText = "いくぞ鬼太郎";
+            } else if(3999 <= time && time <= 4000){
+                game.display.innerText = time / 1000+ "秒" + "さすがじゃ鬼太郎";
+            } else {
+                game.display.innerText = time / 1000 + "秒" + "まだまだじゃな";
+            }
+        }
     };
     //イベントリスナーとボタンを紐ずけ
     game.startButton.onclick = start;
     game.stopButton.onclick = stop;
     game.initButton.onclick = init;
 
-    //表示する時間
-    function showDisplay(time) {
-        if (0 >= time) {
-            game.display.innerText = "いくぞ鬼太郎";
-        } else if(3999 <= time && time <= 4000){
-            game.display.innerText = time / 1000+ "秒" + "さすがじゃ鬼太郎";
-        } else {
-            game.display.innerText = time / 1000 + "秒" + "まだまだじゃな";
-        }
-    }
+
+    // function showDisplay(time) {
+    //     if (0 >= time) {
+    //         game.display.innerText = "いくぞ鬼太郎";
+    //     } else if(3999 <= time && time <= 4000){
+    //         game.display.innerText = time / 1000+ "秒" + "さすがじゃ鬼太郎";
+    //     } else {
+    //         game.display.innerText = time / 1000 + "秒" + "まだまだじゃな";
+    //     }
+    // }
 
     //初期状態は、ストップウォッチが0の状態を表示
     function init() {
         game.initTime = 0;
         game.countUpTime = 0;
         game.timeToAdd = 0;
-        showDisplay(game.initTime);
+        game.showDisplay(game.initTime);
         game.stopButton.disabled = "true";
         game.initButton.disabled = "true";
     }
@@ -50,7 +61,7 @@
         game.countUpId = setTimeout(function () {
             game.countUpTime = Date.now() - game.initTime + game.timeToAdd;
             countUp();
-            showDisplay(game.countUpTime);
+            game.showDisplay(game.countUpTime);
         }, 10);
     }
 
@@ -70,7 +81,7 @@
         //スタート時間に、前回とめた時間を追加する
         game.timeToAdd += Date.now() - game.initTime;
         //ストップを押した時に、判定を追加する
-        showDisplay(game.timeToAdd);
+        game.showDisplay(game.timeToAdd);
         //スタートおした時、ボタンの状態：スタート→押せる、ストップ→押せない
         game.stopButton.disabled = "true";
         game.startButton.disabled = "";
