3fa861fc0b901d2d76729df1124d3b95e3a492f1
bobchin <kinjo.bobchin@gmail.com>
2018-01-24 02:43:01 +0900

    $logと$filterを使ってみた

--- udemy-angularjs/app.js
+++ udemy-angularjs/app.js
@@ -1,5 +1,16 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', function($scope, $log) {
-    console.log($log; //$logが何をしているかコンソールに出力する
+myApp.controller('mainController', function($scope, $log, $filter) {
+    console.log($log); //$logが何をしているかコンソールに出力する
+    $log.log("テストですよ");
+    $log.debug("テストですよ");
+    $log.warn("テストですよ");
+    $log.info("テストですよ");
+    $log.error("テストですよ");
+
+    $scope.name = "ken";
+    $scope.formattedName = $filter('uppercase')($scope.name);
+
+    $log.log($scope.name);
+    $log.log($scope.formattedName);
 });
