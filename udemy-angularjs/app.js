d6e7f4a6c57578fbe4cc85b4a69e48e2789bfeb0
bobchin <kinjo.bobchin@gmail.com>
2018-01-24 02:06:28 +0900

    依存性の注入で、引数がどこにあっても適切に処理する

--- udemy-angularjs/app.js
+++ udemy-angularjs/app.js
@@ -4,4 +4,9 @@ myApp.controller('mainController', function($scope) {
     $scope.name = "テストしすぎさん";
     $scope.hobby = "コードの勉強";
     console.log($scope); //これで$scopeが何をしているかコンソールに出力する
+
+    var people = function(firstName, lastName, age, $scope){
+        return "山田テスト";
+    };
+    console.log(angular.injector().annotate(people));
 });
