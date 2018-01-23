b711fe15d5821a8e2f73f6b2084a1bda7a617840
bobchin <kinjo.bobchin@gmail.com>
2018-01-24 02:21:25 +0900

    試しに$logをコンソールに出力。AngularのAPIリファレンスでどんなサービスが提供されているのかわかる→https://docs.angularjs.org/api

--- udemy-angularjs/app.js
+++ udemy-angularjs/app.js
@@ -1,12 +1,5 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', function($scope) {
-    $scope.name = "テストしすぎさん";
-    $scope.hobby = "コードの勉強";
-    console.log($scope); //これで$scopeが何をしているかコンソールに出力する
-
-    var people = function(firstName, lastName, age, $scope){
-        return "山田テスト";
-    };
-    console.log(angular.injector().annotate(people));
+myApp.controller('mainController', function($scope, $log) {
+    console.log($log; //$logが何をしているかコンソールに出力する
 });
