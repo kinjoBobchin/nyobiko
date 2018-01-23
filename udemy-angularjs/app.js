5eb377f4cb581174d4bc9ebfa1ea5cf3dd372571
bobchin <kinjo.bobchin@gmail.com>
2018-01-24 02:01:47 +0900

    $scopeをconsole.logで出力して、ビューとコントローラがバインドされる様子を確認

--- udemy-angularjs/app.js
+++ udemy-angularjs/app.js
@@ -1,5 +1,7 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', function() {
-
+myApp.controller('mainController', function($scope) {
+    $scope.name = "テストしすぎさん";
+    $scope.hobby = "コードの勉強";
+    console.log($scope); //これで$scopeが何をしているかコンソールに出力する
 });
