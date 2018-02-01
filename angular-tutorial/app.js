f2d46db0a98763c3faba7f4529e72170ea5e097c
bobchin <kinjo.bobchin@gmail.com>
2018-02-01 23:26:12 +0900

    forecastControllerも同様に作成

--- angular-tutorial/app.js
+++ angular-tutorial/app.js
@@ -4,3 +4,7 @@ var weatherApp = angular.module('weatherApp',['ngRoute','ngResource']);
 weatherApp.controller('homeController',['$scope',function($scope) {
 
 }]);
+
+weatherApp.controller('forecastController',['$scope',function($scope) {
+
+}]);
