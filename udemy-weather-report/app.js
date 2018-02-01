289c6009365cac4f4aa6dbf48e5c8f13ea1686ed
金城 裕也 <kinjou_yuya@ca-adv.co.jp>
2018-02-01 19:10:33 +0900

    それぞれのページのrouteを用意した

--- udemy-weather-report/app.js
+++ udemy-weather-report/app.js
@@ -1,6 +1,18 @@
 // moduleの作成
 var weatherApp = angular.module('weatherApp', ['ngRoute','ngResource']);
 
+weatherApp.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'src/home.html',
+            controller: 'homeController'
+        })
+        .when('/forecast', {
+            templateUrl: 'src/forecast.html',
+            controller: 'forecastController'
+        })
+})
+
 weatherApp.controller('homeController',['$scope', function ($scope) {
     
 }]);
