7885cdf079c93ecb41d5fa05cb6a6172121b0237
金城 裕也 <kinjou_yuya@ca-adv.co.jp>
2018-02-01 19:03:11 +0900

    forecastControllerの作成

--- udemy-weather-report/app.js
+++ udemy-weather-report/app.js
@@ -1,6 +1,10 @@
 // moduleの作成
 var weatherApp = angular.module('weatherApp', ['ngRoute','ngResource']);
 
-weatherApp.controller('homeController', ['$scope', function ($scope) {
+weatherApp.controller('homeController',['$scope', function ($scope) {
+    
+}]);
+
+weatherApp.controller('forecastController',['$scope',function($scope) {
     
 }]);
