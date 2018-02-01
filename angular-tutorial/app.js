3d19200c47212a6f92ac73e44c56b76b1deaf0ee
bobchin <kinjo.bobchin@gmail.com>
2018-02-01 23:23:05 +0900

    angularではなく、weatherAppというモジュールのコントローラーに修正

--- angular-tutorial/app.js
+++ angular-tutorial/app.js
@@ -1,6 +1,6 @@
 // moduleの定義
 var weatherApp = angular.module('weatherApp',['ngRoute','ngResource']);
 
-angular.controller('homeController',['$scope',function($scope) {
+weatherApp.controller('homeController',['$scope',function($scope) {
 
 }]);
