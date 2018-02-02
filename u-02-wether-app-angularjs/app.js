be1a5af973d66033c207550519025bb45b8cd2eb
金城 裕也 <kinjou_yuya@ca-adv.co.jp>
2018-02-02 19:35:35 +0900

    ewatherAPIをgetで取りにいく機能を実装したが、エラーが出るので対処が必要。あと、appidをここに書かないでリクエスト出来る方法も探す

--- u-02-wether-app-angularjs/app.js
+++ u-02-wether-app-angularjs/app.js
@@ -2,19 +2,19 @@
 const weatherApp = angular.module('weatherApp',['ngRoute','ngResource']);
 
 //route
-weatherApp.config(function($routeProvider) {
+weatherApp.config(function ($routeProvider) {
     $routeProvider
 
-    .when('/', {
-        templateUrl: 'src/home.html',
-        controller: 'homeController'
-    })
+        .when('/', {
+            templateUrl: 'src/home.html',
+            controller: 'homeController'
+        })
 
-    .when('/forecast', {
-        templateUrl: 'src/forecast.html',
-        controller: 'forecastController'
-    })
-})
+        .when('/forecast', {
+            templateUrl: 'src/forecast.html',
+            controller: 'forecastController'
+        });
+});
 
 //service
 weatherApp.service('cityService', function () {
@@ -25,22 +25,30 @@ weatherApp.service('cityService', function () {
 weatherApp.controller('homeController',['$scope','cityService',function($scope,cityService) {
     $scope.city = cityService.city;
 
-    $scope.$watch('city',function(newCity) {
+    $scope.$watch('city', function (newCity) {
         cityService.city = newCity;
-    })
+    });
 }]);
 
 weatherApp.controller('forecastController',['$scope','cityService','$resource',function($scope,cityService,$resource) {
+
     $scope.city = cityService.city;
 
-    $scope.weatherApi = $resource('http://api.openweathermap.org/data/2.5/forecast', {
+    $scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast', {
         callback: "JSON_CALLBACK"
     }, {
-            get: {
-                method: "JSONP"
-            }
+        get: {
+            method: 'JSONP'
+        }
     });
 
-    $scope.weatherResult = $scope.weatherApi.get();
+    $scope.weatherResult = $scope.weatherAPI.get({
+        q: $scope.city,
+        cnt: 2,
+        units: 'metric',
+        appid: 'f09ccf28addde6486effcc15c32bfaf6'
+    });
+
+    console.log($scope.weatherResult);
 
 }]);
