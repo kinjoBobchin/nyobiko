949141eb1d8ef4bf13a0fc0ee32c21b5c3cbeead
金城 裕也 <kinjou_yuya@ca-adv.co.jp>
2018-02-06 15:29:17 +0900

    このままでは呼べないので、何かしらのメソッドを用意して、よぶ形にしたいので、getWeatherでつつむ

--- u-02-wether-app-angularjs/service/weather-service.js
+++ u-02-wether-app-angularjs/service/weather-service.js
@@ -2,20 +2,24 @@
 
 weatherApp.service('weatherService',['$http','$sce','$httpParamSerializerJQLike', function ($http, $sce, $httpParamSerializerJQLike) {
 
-    // trustAsResourceUrlにいれるまでは、まだセキュアなURLとして使用できない
-    const unTrustedUrl = 'http://api.openweathermap.org/data/2.5/forecast/';
+    this.getWeather = function(){
 
-    // unTrustedUrlにくっつけるパラメータを定義
-    const params = $httpParamSerializerJQLike({
-        q: $scope.city + ",jp", //検索した都市名に国名をくっつけている
-        cnt: $scope.days,
-        units: 'metric',
-        appid: 'f09ccf28addde6486effcc15c32bfaf6'
-    });
+        // trustAsResourceUrlにいれるまでは、まだセキュアなURLとして使用できない
+        const unTrustedUrl = 'http://api.openweathermap.org/data/2.5/forecast/';
 
-    const promise = $http.jsonp($sce.trustAsResourceUrl(unTrustedUrl + '?' + params));
-    promise.then(function(jsonp){
-        $scope.weatherResult = jsonp;
-    });
+        // unTrustedUrlにくっつけるパラメータを定義
+        const params = $httpParamSerializerJQLike({
+            q: $scope.city + ",jp", //検索した都市名に国名をくっつけている
+            cnt: $scope.days,
+            units: 'metric',
+            appid: 'f09ccf28addde6486effcc15c32bfaf6'
+        });
+
+        const promise = $http.jsonp($sce.trustAsResourceUrl(unTrustedUrl + '?' + params));
+        promise.then(function(jsonp){
+            $scope.weatherResult = jsonp;
+        });
+
+    };
 
 }]);
