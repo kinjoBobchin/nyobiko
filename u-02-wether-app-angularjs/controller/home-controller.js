71aac1767af4a85791fb2b4728c90da3b6554cbc
金城 裕也 <kinjou_yuya@ca-adv.co.jp>
2018-02-06 13:05:02 +0900

    ngSubmitでsubmit関数を呼び出したので、それをコントローラーに作成

--- u-02-wether-app-angularjs/controller/home-controller.js
+++ u-02-wether-app-angularjs/controller/home-controller.js
@@ -1,8 +1,12 @@
 //controller
-weatherApp.controller('homeController',['$scope','cityService',function($scope,cityService) {
+weatherApp.controller('homeController',['$location','$scope','cityService',function($location,$scope, cityService) {
     $scope.city = cityService.city;
 
     $scope.$watch('city', function (newCity) {
         cityService.city = newCity;
     });
+
+    $scope.submit = function () {
+        $location.path('/forecast');
+    };
 }]);
