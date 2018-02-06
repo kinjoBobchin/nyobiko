58176139bbc8059450ed1a358f19998b0e8cfcb5
金城 裕也 <kinjou_yuya@ca-adv.co.jp>
2018-02-06 15:18:41 +0900

    forecast-controllerから、天気情報をとってきている部分を移植。 これから書き換えていく。

--- u-02-wether-app-angularjs/service/weather-service.js
+++ u-02-wether-app-angularjs/service/weather-service.js
@@ -1,5 +1,21 @@
 // open weatherのサイトからAPIで天気情報をとってくる
 
-weaterApp.service('getWeatherService', function() {
-    
+weaterApp.service('weatherService', function () {
+
+    // trustAsResourceUrlにいれるまでは、まだセキュアなURLとして使用できない
+    const unTrustedUrl = 'http://api.openweathermap.org/data/2.5/forecast/';
+
+    // unTrustedUrlにくっつけるパラメータを定義
+    const params = $httpParamSerializerJQLike({
+        q: $scope.city + ",jp", //検索した都市名に国名をくっつけている
+        cnt: $scope.days,
+        units: 'metric',
+        appid: 'f09ccf28addde6486effcc15c32bfaf6'
+    });
+
+    const promise = $http.jsonp($sce.trustAsResourceUrl(unTrustedUrl + '?' + params));
+    promise.then(function(jsonp){
+        $scope.weatherResult = jsonp;
+    });
+
 });
