0723797c0ce56e8ba9d11dd4980cb878cc208c31
bobchin <kinjo.bobchin@gmail.com>
2018-02-07 23:31:48 +0900

    weatherServiceからリクエストしたデータを受け取るように変更

--- u-02-wether-app-angularjs/controller/forecast-controller.js
+++ u-02-wether-app-angularjs/controller/forecast-controller.js
@@ -6,6 +6,9 @@ weatherApp.controller('forecastController', ['$scope', 'cityService', 'weatherSe
     // 何日分のデータをとってくるかをパラメで受け取る
     $scope.days = $routeParams.days || '2';
 
+    $scope.weatherResult = weatherService.getWeather($scope.city, $scope.days);
+    console.log($scope.weatherResult);
+
     $scope.convertDate = function (date) {
         return new Date(date * 1000);
     };
