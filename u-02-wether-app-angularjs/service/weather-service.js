d4f55a372e4e8e2a533ffd786bb3e045daed27aa
bobchin <kinjo.bobchin@gmail.com>
2018-02-07 23:31:05 +0900

    $resourceを使ってエラーなしで作成することができた！

--- u-02-wether-app-angularjs/service/weather-service.js
+++ u-02-wether-app-angularjs/service/weather-service.js
@@ -1,23 +1,16 @@
 // open weatherのサイトからAPIで天気情報をとってくる
 
-weatherApp.service('weatherService',['$http','$sce','$httpParamSerializerJQLike', function ($http, $sce, $httpParamSerializerJQLike) {
+weatherApp.service('weatherService',['$http','$sce','$httpParamSerializerJQLike','$resource', function ($http, $sce, $httpParamSerializerJQLike, $resource) {
 
     this.getWeather = function(city, days){
 
-        // trustAsResourceUrlにいれるまでは、まだセキュアなURLとして使用できない
-        const unTrustedUrl = 'http://api.openweathermap.org/data/2.5/forecast/';
+        const weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast');
 
-        // unTrustedUrlにくっつけるパラメータを定義
-        const params = $httpParamSerializerJQLike({
-            q: city + ",jp", //検索した都市名に国名をくっつけている
+        return weatherResult = weatherAPI.get({
+            q: city + ",jp",
             cnt: days,
             units: 'metric',
             appid: 'f09ccf28addde6486effcc15c32bfaf6'
-        });
-
-        const promise = $http.jsonp($sce.trustAsResourceUrl(unTrustedUrl + '?' + params));
-        promise.then(function(jsonp){
-            return weatherResult = jsonp;
         });
 
     };
