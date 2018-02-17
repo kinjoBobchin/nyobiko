c2ff7ecd05c1ef610fffa20f2c0894ac76a9a585
bobchin <kinjo.bobchin@gmail.com>
2018-02-18 00:35:42 +0900

    １、プラグインを追加するための、requireを追加 ２、output先のpathを記載。__dirnameがないと、「The output directory as **absolute path**(required).」と言われて怒られる ３、plugins: [new HtmlWebpackPlugin()]を追記する

--- y-01-webpack-tutorial/webpack.config.js
+++ y-01-webpack-tutorial/webpack.config.js
@@ -1,6 +1,10 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: './src/app.js',
     output: {
-        filename: './dist/app.bundle.js'
-    }
+        path: __dirname + '/dist',
+        filename: 'app.bundle.js'
+    },
+    plugins: [new HtmlWebpackPlugin()]
 };
