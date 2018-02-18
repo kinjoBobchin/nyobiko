3a8744c628111efd39d4e8735b95f536d136e58d
bobchin <kinjo.bobchin@gmail.com>
2018-02-19 00:59:11 +0900

    ①portは指定しなければ、port8081がデフォルトになるようです ②openをtrueにすると、serverをたちあげたタイミングでブラウザを開いてくれます

--- y-01-webpack-tutorial/webpack.config.js
+++ y-01-webpack-tutorial/webpack.config.js
@@ -22,8 +22,8 @@ module.exports = {
     devServer: {
         contentBase: path.join(__dirname, "dist"),
         compress: true,
-        port: 9000,
-        stats: "errors-only"
+        stats: "errors-only",
+        open: true
     },
     plugins: [
         new HtmlWebpackPlugin({
