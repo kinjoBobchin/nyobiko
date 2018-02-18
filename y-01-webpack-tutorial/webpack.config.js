20b8b59fe494dd31560e6311f243f84c78270496
bobchin <kinjo.bobchin@gmail.com>
2018-02-18 12:30:25 +0900

    css-loaderを使用するのをwebpack.confi.jsonに追加

--- y-01-webpack-tutorial/webpack.config.js
+++ y-01-webpack-tutorial/webpack.config.js
@@ -6,6 +6,11 @@ module.exports = {
         path: __dirname + '/dist',
         filename: 'app.bundle.js'
     },
+    module:{
+        rules: [
+            {test: /\.css$, use: 'css-loader'}
+        ],
+    },
     plugins: [new HtmlWebpackPlugin({
         title: 'yeah template',
         minify: {
