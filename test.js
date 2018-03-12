f3af6e844479bd232845165a06152ac84e9d45d5
金城 裕也 <kinjou_yuya@ca-adv.co.jp>
2018-03-12 17:05:37 +0900

    キーワードの前にスペースがないとアラートを出す設定を追加

--- test.js
+++ test.js
@@ -1,1 +1,15 @@
-console.log('hello');
+console.log("hello");
+
+// keywords
+if (a) {
+  b();
+}
+
+
+ // functions
+var a = function() {}
+
+// classes
+class Foo {
+  constructor() {} // functions
+}
